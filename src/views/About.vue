69a96dd8a0b8e0a9c792b6e52d40a8f325c659ea
devguchi <devguchitaro@gmail.com>
2021-12-04 15:40:36 +0900

    added blockNumber and Balance

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1><a href="https://docs.ethers.io/">ethers.js</a> Test</h1>
+    <input v-model="address" placehodler="Ethereum Address" />
+    <button @click="getBalance">balance</button>
+    <dl>
+      <dt>Block Number</dt>
+      <dd>{{blockNumber}}</dd>
+    </dl>
+    <dl>
+      <dt>Balance</dt>
+      <dd>{{balance}}</dd>
+    </dl>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import {ethers} from "ethers";
+import {markRaw} from "vue";
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
+export default defineComponent({
+  data: () => ({
+    provider: null as any,
+    blockNumber: null as number | null,
+    address: null as string | null,
+    balance: 0.0 as number
+  }),
+  async created() {
+    await window.ethereum.enable();
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    this.provider = markRaw(provider);
+    this.getBlockNumber();
+  },
+  methods: {
+    async getBlockNumber() {
+      this.blockNumber = await this.provider.getBlockNumber();
+    },
+    async getBalance() {
+      this.balance = 0.0
+      const balance = await this.provider.getBalance(this.address);
+      this.balance = parseFloat(ethers.utils.formatEther(balance));
+    }
+  }
+})
+</script>
