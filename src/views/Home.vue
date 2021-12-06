3809204e8281160e4d2ed6e37b687027042c9fd2
devguchi <devguchitaro@gmail.com>
2021-12-06 13:43:34 +0900

    added bytes32 to string

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  bytes32 <input v-model="b32" size="80" /><br />
+  string <input v-model="str" size="80" style="margin-top: 10px" /><br />
+  <button @click="toString" style="margin-top: 10px">toString</button>&nbsp;
+  <button @click="toBytes32">toBytes32</button>
 </template>
 
 <script lang="ts">
+import { BytesLike, ethers } from "ethers";
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: () => ({
+    b32: null as BytesLike | null,
+    str: null as string | null,
+  }),
+  methods: {
+    toString() {
+      if (this.b32 === null) return;
+      this.str = ethers.utils.parseBytes32String(this.b32);
+    },
+    toBytes32() {
+      if (this.str === null) return;
+      this.b32 = ethers.utils.formatBytes32String(this.str);
+    },
   },
 });
 </script>
